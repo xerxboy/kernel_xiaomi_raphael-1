fa8b966801f5cdc14685e8b85151e352ffce5d91
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-25 04:38:14 +0900

    backlight: remove unnecessary counting
    use_count are meant to filter out framebuffer's, which not actually
    happened here.
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- drivers/video/backlight/backlight.c
+++ drivers/video/backlight/backlight.c
@@ -182,14 +182,6 @@ int backlight_device_set_brightness(struct backlight_device *bd,
 		if (brightness > bd->props.max_brightness)
 			rc = -EINVAL;
 		else {
-			if ((!bd->use_count && brightness) || (bd->use_count && !brightness)) {
-				pr_debug("%s: set brightness to %lu\n", __func__, brightness);
-				if (!bd->use_count)
-					bd->use_count++;
-				else
-					bd->use_count--;
-			}
-
 			pr_debug("set brightness to %lu\n", brightness);
 			if (!brightness) {
 				bl_event = BACKLIGHT_OFF;
