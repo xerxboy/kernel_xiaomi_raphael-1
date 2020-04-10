F1xy-kernels/RAPHAEL_Kernel

cpu_cooling: fix a throttling bug
Without this change throttling would get stuck in enabled state forever.
This leads to a regression in Geekbench 5 single-core score
from 760 to 620 on 2-3 run, forever.

Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

drivers/thermal/cpu_cooling.c
@@ -301,7 +301,7 @@ static int cpufreq_thermal_notifier(struct notifier_block *nb,
 	unsigned long clipped_freq = ULONG_MAX, floor_freq = 0;
 	struct cpufreq_cooling_device *cpufreq_cdev;
 
-	if (event != CPUFREQ_THERMAL)
+	if (event != CPUFREQ_THERMAL || event != CPUFREQ_INCOMPATIBLE)
 		return NOTIFY_DONE;
 
 	mutex_lock(&cooling_list_lock);

xerxboy/kernel_xiaomi_raphael-1

Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

drivers/thermal/cpu_cooling.c
@@ -301,7 +301,7 @@ static int cpufreq_thermal_notifier(struct notifier_block *nb,
 	unsigned long clipped_freq = ULONG_MAX, floor_freq = 0;
 	struct cpufreq_cooling_device *cpufreq_cdev;
 
-	if (event != CPUFREQ_THERMAL)
+	if (event != CPUFREQ_INCOMPATIBLE || event != CPUFREQ_THERMAL)
 		return NOTIFY_DONE;
 
 	mutex_lock(&cooling_list_lock);